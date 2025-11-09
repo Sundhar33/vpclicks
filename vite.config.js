fe7a2be3cf054e223519e887c5b11f7d9a371f13
Sundhara Chozhan k <810422243311@dsengg.ac.in>
2025-11-09 20:20:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: "dist", // 👈 Vercel looks for this folder by default
+  },
+  // 👇 This makes sure all routes (React Router, etc.) work correctly
+  base: "./",
 });
